3de8b89fcafa134f703d2e88243580e55c1de5ee
radzvpn <40193115+radzvpn@users.noreply.github.com>
2019-07-18 17:58:20 +0800

    Update pinoytheme.sh

--- pinoytheme.sh
+++ pinoytheme.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/vps/public_html/asset
-rm -rf /home/vps/public_html/view
+rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/view
 rm -rf /home/vps/public_html/tmp/*
 echo "Installing RadzVPN Theme"
 cd ~
-mkdir Sleak
-cd Sleak
+mkdir Radz
+cd Radz
 rm -rf *
 wget https://www.dropbox.com/s/7yxkqg64zi5rjno/radz-keen.tgz?dl=0 tar zxvf radz-keen.tgz
-mv asset /home/vps/public_html
-mv view /home/vps/public_html
+mv asset /home/panel/html
+mv view /home/panel/html
 echo "Enjoy using RadzVPN Theme"

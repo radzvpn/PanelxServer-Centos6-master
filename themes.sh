ef46ec648932305db483747ee6e8ecfb821ab781
radzvpn <40193115+radzvpn@users.noreply.github.com>
2019-09-04 18:12:31 +0800

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/vps/public_html/asset
-rm -rf /home/vps/public_html/view
-rm -rf /home/vps/public_html/tmp/*
+rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/view
+rm -rf /home/panel/html/tmp/*
 echo "Installing RadzVPN Themes"
 cd ~
 mkdir Sleak
 cd Sleak
 rm -rf *
 wget https://www.dropbox.com/s/tve2i47aojynj4g/radzpanel.tar.gz && tar zxvf radzpanel.tgz
-mv asset /home/vps/public_html
-mv view /home/vps/public_html
+mv asset /home/panel/html
+mv view /home/panel/html
 echo "Enjoy using RadzVPN Themes"
